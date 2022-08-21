07900096144132cdeb31c3c3cad54afbd833489f
AlanHeck <100172228+AlanHeck@users.noreply.github.com>
2022-08-21 14:04:32 -0300

    se agrega cart e itemlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
+import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-    <h1>Burgers</h1>
+      <h1>Heck Burgers</h1>
+      <ItemListContainer contenedor='Lista contenedora'/>  
     </div>
   );
 }
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItemListContainer = (props) => {
+  return (
+    <div>{props.contenedor}</div>
+  )
+}
+
+export default ItemListContainer
